772d31c17eff9a4bbdec3d8b99e0c90a1db995e3
6xssStxrrfall <puedbb07@gmail.com>
2026-09-12 17:33:14 +0700

    0.2

--- lib/views/login_ui.dart
+++ lib/views/login_ui.dart
@@ -11,7 +11,32 @@ class _LoginUIState extends State<LoginUI> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 65, 84, 189),
+      backgroundColor: const Color.fromARGB(255, 241, 241, 241),
+      body: Center(
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: OutlinedButton(
+                onPressed: () {},
+                child: Icon(
+                  Icons.arrow_back_ios_new_sharp,
+                  size: 25.0,
+                ),
+                style: OutlinedButton.styleFrom(
+                  fixedSize: Size(
+                    75.0,
+                    65.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
